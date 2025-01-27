74f5fa19d9766c5a200ad9f4c59b8a2e5d33e0fa
Olamilekan <adebukolaolamilekan123@gmail.com>
2025-01-27 20:28:53 +0100

    úpdating home page hero section

--- OpenAI-Image-Analyzer/client/src/components/Header.jsx
+++ OpenAI-Image-Analyzer/client/src/components/Header.jsx
@@ -3,31 +3,31 @@ import { FaXTwitter } from "react-icons/fa6";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-1/2 transform -translate-x-1/2 w-[60%] bg-black text-white p-1 shadow-md z-47 rounded-full mt-7 font-poppins">
+    <header className="fixed top-0 left-1/2 transform -translate-x-1/2 w-[90%] md:w-[60%] bg-black text-white p-1 shadow-md z-47 rounded-full mt-4 md:mt-7 font-poppins">
       <div className="flex justify-between items-center">
-        <h1 className="text-2xl font-semibold hover:bg-white hover:text-black py-1 px-3 rounded-l-full border-2 border-white text-white rounded-br-full bg-black transition duration-300">
-          SpectraCore<span className="r text-lg align-top">&reg;</span>
+        <h1 className="text-lg md:text-2xl font-semibold hover:bg-white hover:text-black py-1 px-2 md:px-3 rounded-l-full border-1 md:border-2 border-white text-white rounded-br-full bg-black transition duration-300 cursor-pointer">
+          SpectraCore<span className="r text-sm align-top">&reg;</span>
         </h1>
 
-        <ul className="border-b-1 py-2 px-7 rounded-full shadow-md space-x-7 flex justify-center items-center">
-          <li className="border-r-1 pr-7 border-white ">
+        <ul className="border-b-[0.5px] md:border-b-1 py-2 px-2 md:px-7 rounded-full shadow-md space-x-2 md:space-x-7 flex justify-center items-center">
+          <li className="border-r-1 text-sm md:text-md pr-2 md:pr-7 border-white">
             <a href="#about">Docs</a>
           </li>
-          <li className="border-r-1 pr-7 border-white ">
+          <li className="border-r-1 text-sm md:text-md pr-2 md:pr-7 border-white">
             <a href="#about">Github</a>
           </li>
-          <li className="border-r-1 pr-7 border-white ">
-            <a href="#about">Contact Us</a>
+          <li className="border-r-1 text-sm md:text-md pr-2 md:pr-7 border-white">
+            <a href="#contact">Contact Us</a>
           </li>
           <li className="">
-            <a href="#about"><FaXTwitter /></a>
+            <a href="https://x.com/"><FaXTwitter /></a>
           </li>
         </ul>
 
-        <button className="hover:bg-white py-2 px-3 rounded-full shadow-md hover:text-black font-semibold  border-white border-2 bg-black text-white transition duration-300">
-          <Link to="/Analyze">Try $IRISAI</Link>
+        <button className="hover:bg-white py-1 md:py-2 px-1.5 md:px-3 rounded-full shadow-md hover:text-black font-semibold  border-white border-2 bg-black text-white transition duration-300">
+          <Link to="/Analyze">Try $IRISAI<span className="r text-sm align-top">&reg;</span></Link>
         </button>
-      </div>
+        </div>
     </header>
   );
 };
--- OpenAI-Image-Analyzer/client/src/components/Hero.jsx
+++ OpenAI-Image-Analyzer/client/src/components/Hero.jsx
@@ -1,12 +1,7 @@
 const Hero = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-black text-center">
-      <h1 className="text-5xl font-bold font-popins">
-        Smarter Insights, <br />
-        Clear Vision <br />
-        with SpectraCore
-      </h1>
-      <h1 className="text-5xl font-bold">
+    <div className="flex flex-col items-center justify-center h-screen text-black text-center gap-5">
+      <h1 className="text-7xl font-bold cursor-text">
         {Array.from("Smarter Insights, Clear Vision").map(
           (char, index) => (
             <span
@@ -18,7 +13,7 @@ const Hero = () => {
           )
         )}
       </h1>
-      <h1 className="text-5xl font-bold">
+      <h1 className="text-7xl font-bold cursor-text">
         {Array.from("with SpectraCore").map(
           (char, index) => (
             <span
@@ -30,6 +25,18 @@ const Hero = () => {
           )
         )}
       </h1>
+
+      <button id="try AI btn" className="text-2xl mt-5 font-semibold hover:bg-transparent hover:text-black py-2 px-4 rounded-full border-3 border-white hover:border-black text-white rounded-br-full bg-black transition duration-300 cursor-pointer">
+          Try our $IRISAI<span className="r text-md align-top">&reg;</span>
+        </button>
+
+        <p id="CA" className="text-gray-700 font-semibold text-lg">
+          Try $IRISAI<span className="r text-sm align-top">&reg;</span> CA: CyQr3CGBDd4gTncX4qSRMB4wQqQyRLY5iDgKHjhXpump
+        </p>
+
+        <h1 id="bottom" className="fixed bottom-0 left-0 text-3xl font-semibold hover:bg-white hover:text-black py-1 px-3 rounded-r-full border-2 border-white text-white rounded-bl-full bg-black transition duration-300 cursor-pointer">
+          SpectraCore<span className="r text-lg align-top">&reg;</span>
+        </h1>
     </div>
   );
 };
