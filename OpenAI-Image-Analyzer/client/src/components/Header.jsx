2a2302b380c3dbfeacbf0229c4bce8ff1ffc0402
Olamilekan <adebukolaolamilekan123@gmail.com>
2025-01-27 20:34:42 +0100

    úpdating home page hero section

--- OpenAI-Image-Analyzer/client/src/components/Header.jsx
+++ OpenAI-Image-Analyzer/client/src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
           SpectraCore<span className="r text-sm align-top">&reg;</span>
         </h1>
 
-        <ul className="border-b-[0.5px] md:border-b-1 py-2 px-2 md:px-7 rounded-full shadow-md space-x-2 md:space-x-7 flex justify-center items-center">
+        <ul className="border-b-[0.5px] md:border-b-1 py-2 px-2 md:px-7 rounded-full shadow-md space-x-2 md:space-x-7 flex justify-center items-center hidden lg:block">
           <li className="border-r-1 text-sm md:text-md pr-2 md:pr-7 border-white">
             <a href="#about">Docs</a>
           </li>
@@ -24,7 +24,7 @@ const Header = () => {
           </li>
         </ul>
 
-        <button className="hover:bg-white py-1 md:py-2 px-1.5 md:px-3 rounded-full shadow-md hover:text-black font-semibold  border-white border-2 bg-black text-white transition duration-300">
+        <button className="hover:bg-white md:text-sm py-1 md:py-2 px-1.5 md:px-3 rounded-full shadow-md hover:text-black font-semibold  border-white border-1 md:border-2 bg-black text-white transition duration-300">
           <Link to="/Analyze">Try $IRISAI<span className="r text-sm align-top">&reg;</span></Link>
         </button>
         </div>
