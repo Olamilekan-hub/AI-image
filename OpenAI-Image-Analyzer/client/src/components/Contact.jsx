80475101ce06f8795e6564e6b3346e8284aeef6b
Olamilekan <adebukolaolamilekan123@gmail.com>
2025-01-29 00:57:53 +0100

    updating

--- OpenAI-Image-Analyzer/client/src/components/Contact.jsx
+++ OpenAI-Image-Analyzer/client/src/components/Contact.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaPaperPlane, FaSpinner, FaCheck } from "react-icons/fa";
 
 const ContactForm = () => {
   const [status, setStatus] = useState("");
@@ -7,6 +8,7 @@ const ContactForm = () => {
     username: "",
     message: "",
   });
+  const [buttonState, setButtonState] = useState("initial");
 
   // Handle form input changes
   const handleChange = (e) => {
@@ -17,6 +19,7 @@ const ContactForm = () => {
   // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setButtonState("loading");
 
     try {
       const response = await fetch(
@@ -30,39 +33,80 @@ const ContactForm = () => {
 
       if (response.ok) {
         setStatus("Message sent successfully!");
+        setTimeout(() => setStatus(""), 5000);
+
         setFormData({ name: "", username: "", message: "" }); // Reset the form
+        setButtonState("sent");
+
+        // Reset the button state after 5 seconds
+        setTimeout(() => setButtonState("initial"), 5000);
       } else {
         setStatus("Failed to send message.");
+        setButtonState("initial");
       }
     } catch (error) {
       console.log(error);
       setStatus(`${error}: An error occurred. Please try again.`);
+      setButtonState("initial");
+    }
+  };
+
+  // Render button content dynamically based on the buttonState
+  const renderButtonContent = () => {
+    if (buttonState === "loading") {
+      return (
+        <>
+          Sending...
+          <FaSpinner className="animate-spin mr-2" />
+        </>
+      );
+    } else if (buttonState === "sent") {
+      return (
+        <>
+          Sent
+          <FaCheck className="mr-2" />
+        </>
+      );
+    } else {
+      return (
+        <>
+          Send
+          <FaPaperPlane className="mr-2" />
+        </>
+      );
     }
   };
 
   return (
     <div id="contact" className="py-12 bg-gray-900 items-center">
-      <h2 className="text-7xl font-bold mb-2 text-gray-200 text-center">       
+      <h2 className="font-bold mb-2 text-gray-200 text-center cf  lg:text-7xl  md:text-6xl  text-5xl">
         Contact Us
-      </h2> 
+      </h2>
       <hr className="border-1 border-gray-700 mb-6" />
 
-      <div className="flex flex-col lg:flex-row justify-between items-center px-8">
-       {/* Contact Image */}
-          <div className="order-2 lg:order-0 w-[95%] lg:w-[45%] lg:ml-8 flex justify-center items-center">
-            <img src="/images/contact-img.svg" className="w-[98%] hover:w-full transition duration-700 ease-out-out" alt="contact_svg" />
-          </div>
+      <div className="flex flex-col lg:flex-row justify-between items-center px-8 gap-10 lg:gap-0">
+        {/* Contact Image */}
+        <div className="order-2 lg:order-0 w-[95%] lg:w-[45%] lg:ml-8 flex justify-center items-center">
+          <img
+            src="/images/contact-img.svg"
+            className="w-[98%] hover:w-full transition duration-700 ease-out-out"
+            alt="contact_svg"
+          />
+        </div>
 
-          {/* Contact Form */}
+        {/* Contact Form */}
         <form
           onSubmit={handleSubmit}
-          className="w-[40%] mx-auto p-6 bg-gray-100 shadow-md shadow-gray-400 rounded-2xl"
+          className="lg:w-[40%] w-full lg:mx-auto p-6 bg-gray-100 shadow-md shadow-gray-400 rounded-2xl"
         >
-          {status && <p className="text-sm text-green-600">{status}</p>}
+          {status && <p className="text-md text-green-600">{status}</p>}
 
           {/* name */}
           <div className="mb-5">
-            <label htmlFor="name" className="block text-2xl font-medium text-gray-900 mb-2 ">
+            <label
+              htmlFor="name"
+              className="block text-2xl font-medium text-gray-900 mb-2 "
+            >
               Name
             </label>
             <input
@@ -72,14 +116,17 @@ const ContactForm = () => {
               value={formData.name}
               onChange={handleChange}
               placeholder="Enter your name..."
-              className="text-gray-900 w-full border border-gray-700 p-2 rounded-full placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-900 shadow-sm shadow-gray-500 "
+              className="text-gray-900 w-full border border-gray-700 py-2 px-4 rounded-full placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-900 shadow-sm shadow-gray-500 "
               required
             />
           </div>
 
           {/* username */}
           <div className="mb-5">
-            <label htmlFor="username" className="block text-xl font-medium text-gray-900 mb-2 ">
+            <label
+              htmlFor="username"
+              className="block text-xl font-medium text-gray-900 mb-2 "
+            >
               Email or Telegram/Twitter(X) Username
             </label>
             <input
@@ -89,14 +136,17 @@ const ContactForm = () => {
               value={formData.username}
               onChange={handleChange}
               placeholder="Enter your username (eg. @username) or email..."
-              className="text-gray-900 w-full border border-gray-700 p-2 rounded-full placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-900 shadow-sm shadow-gray-500 "
+              className="text-gray-900 w-full border border-gray-700 py-2 px-4 rounded-full placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-900 shadow-sm shadow-gray-500 "
               required
             />
           </div>
 
           {/* message */}
           <div className="mb-4">
-            <label htmlFor="message" className="block text-2xl font-medium text-gray-900 mb-2 ">
+            <label
+              htmlFor="message"
+              className="block text-2xl font-medium text-gray-900 mb-2 "
+            >
               Message
             </label>
             <textarea
@@ -105,8 +155,8 @@ const ContactForm = () => {
               value={formData.message}
               onChange={handleChange}
               placeholder="Enter your message..."
-              className="text-gray-900 w-full border border-gray-700 p-2 rounded-lg placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-900 shadow-sm shadow-gray-500 "
-              rows="5"
+              className="text-gray-900 w-full border border-gray-700 py-2 px-4 rounded-xl placeholder-gray-500 focus:outline-none focus:ring-1 focus:ring-gray-900 shadow-sm shadow-gray-500 "
+              rows="4"
               required
             />
           </div>
@@ -114,9 +164,9 @@ const ContactForm = () => {
           {/* submit */}
           <button
             type="submit"
-            className="bg-blue-500 text-gray-900 py-2 px-4 rounded-full transition duration-700 ease-in-out"
+            className="bg-gray-500 font-semibold text-gray-900 py-1 px-6 rounded-2xl transition duration-700 ease-in-out hover:bg-gray-700 hover:text-gray-100 text-lg hover:text-xl flex items-center justify-center gap-2 hover:py-2 mb-4 mt-2"
           >
-            Send
+            {renderButtonContent()}
           </button>
         </form>
       </div>
