cf8262b84f1e7467af324eb651fa56e9a155b35b
Olamilekan <adebukolaolamilekan123@gmail.com>
2025-02-02 12:52:10 +0100

    Vite redirecting

--- OpenAI-Image-Analyzer/client/vite.config.js
+++ OpenAI-Image-Analyzer/client/vite.config.js
@@ -5,4 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  root: '/',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      },
+    },
+  },
+  server: {
+    historyApiFallback: true,
+  },
 });
